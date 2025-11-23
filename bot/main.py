6306cc87b2700daf41084b7fafa0bcda51286010
pramod134 <37002662+pramod134@users.noreply.github.com>
2025-11-23 00:44:58 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,17 @@
 import asyncio
 
-from .loops import run_positions_loop, run_quotes_loop
+from .loops import (
+    run_positions_loop,
+    run_quotes_loop,
+    run_spot_indicators_loop,
+)
 
 
 async def main() -> None:
     await asyncio.gather(
         run_positions_loop(),
         run_quotes_loop(),
+        run_spot_indicators_loop(),
     )
 
 
